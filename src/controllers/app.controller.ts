83daf1b9436c5f1b52de668b7113fce6f9900bd7
Jan Bílek <honza@elision.design>
2019-03-20 15:23:45 +0100

    Use promise to resolve api requests

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { MeteoStationService } from '../services/meteo-station.service';
-import { MeteoStation } from 'src/entities/meteo-station.entity';
 import { GeoLocateService } from '@/services/geo-locate.service';
-import { AxiosResponse } from 'axios';
 
 @Controller()
 export class AppController {
@@ -12,15 +10,8 @@ export class AppController {
   ) { }
 
   @Get()
-  getHello() {
-    return this.geoService.findIp('234');
-  }
-  @Get("nearest")
-  async getNearest() {
-    return await this.meteoService.findNearest();
-  }
-  @Get("raw")
-  async getRaw() {
-    return await this.meteoService.rawQuery();
+  async getHello() {
+    const data$ = await this.geoService.locateIp('');
+    return data$;
   }
 }
--- src/services/geo-locate.service.ts
+++ src/services/geo-locate.service.ts
@@ -1,16 +1,15 @@
 import { Injectable, HttpService } from '@nestjs/common';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Point } from '@/models/point.model';
 
 @Injectable()
 export class GeoLocateService {
     constructor(
         private readonly http: HttpService,
     ) { }
-    
-    findIp(ip: string): object {
-        return this.http.get('http://ip-api.com/json/').pipe(
-            map(response => response.data)
-        );
+
+    async locateIp(ip: string): Promise<object> {
+        const data =  await this.http.get(`http://ip-api.com/json/${ip}`).toPromise();
+        return data.data;
     }
+
 }
