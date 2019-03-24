ab63eb04ebf217e0ad7f2867ca27013f86ce4ca8
Jan Bílek <honza@elision.design>
2019-03-24 23:50:37 +0100

    Add ip geolocation test

--- src/services/geo-locate.service.spec.ts
+++ src/services/geo-locate.service.spec.ts
@@ -1,7 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GeoLocateService } from './geo-locate.service';
-import { HttpService } from '@nestjs/common';
+import { HttpService, HttpException } from '@nestjs/common';
 import { CacheService } from './cache.service';
+import { of, Observable } from 'rxjs';
+import { AxiosResponse, AxiosRequestConfig } from 'axios';
+
+
+class CacheServiceMock {
+  get(key: string | number) { return ''; }
+
+  set(key: string | number, value: any) { }
+
+  delete(keys: string | number | []) { }
+
+  flush() { }
+}
+
+class HttpServiceMock {
+  request(config: AxiosRequestConfig) { };
+  get(url: string, config?: AxiosRequestConfig) { };
+  delete(url: string, config?: AxiosRequestConfig) { }
+  head(url: string, config?: AxiosRequestConfig) { }
+  post(url: string, data?: any, config?: AxiosRequestConfig) { }
+  put(url: string, data?: any, config?: AxiosRequestConfig) { }
+  patch(url: string, data?: any, config?: AxiosRequestConfig) { }
+}
 
 describe('GeoLocateService', () => {
   let http: HttpService;
@@ -14,27 +37,35 @@ describe('GeoLocateService', () => {
         GeoLocateService,
         {
           provide: HttpService,
-          useValue: ,
+          useValue: new HttpServiceMock(),
         },
         {
           provide: CacheService,
-          useClass: CacheService,
+          useValue: new CacheServiceMock(),
         },
       ],
     }).compile();
 
     http = module.get<HttpService>(HttpService);
     service = module.get<GeoLocateService>(GeoLocateService);
-    
+    cache = module.get<CacheService>(CacheService);
+
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return ip location', () => {
-    // cache = jest.spyOn(cache, "get")
-    // .mockReturnValueOnce(null)
-    // .mockReturnValue(Promise.resolve({ lat: 50, lng: 14.49 }) as Promise<Coordinates>);
-  })
+  it('should return ip location', async () => {    
+    const coordinates = { lat: 50, lng: 14.6666 };
+    const response: AxiosResponse = { data: { lat: 50, lon: 14.6666 }, status: 200, statusText: '', headers: {}, config: {} };
+    jest.spyOn(cache, 'get').mockImplementationOnce(() => null).mockImplementationOnce(() => coordinates);
+    jest.spyOn(http, 'get').mockImplementation(() => of(response));
+
+    const location = await service.getIpLocation('8.8.8.8');
+    expect(location.lat).toBe(coordinates.lat);
+    expect(location.lng).toBe(coordinates.lng);
+ 
+  });
+
 });
